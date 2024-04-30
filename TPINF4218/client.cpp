d48a65cf92269dcad2a9715525d12de0640b872e
jbbonice2 <128254349+jbbonice2@users.noreply.github.com>
2024-04-30 06:30:23 +0100

    Update client.cpp

--- TPINF4218/client.cpp
+++ TPINF4218/client.cpp
@@ -1,159 +1,154 @@
 #include <iostream>
-#include <filesystem>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <unistd.h>
-#include <sys/inotify.h>
-#include <syslog.h> // Ajout de l'en-tête pour la bibliothèque syslog
+#include <arpa/inet.h>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <unordered_map>
+#include <filesystem>
+#include <syslog.h>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <cstdlib> // For atoi
+#include <cstring> // For strlen
 
 namespace fs = std::filesystem;
 
-const int BUF_LEN = (1024 * (sizeof(struct inotify_event) + 16));
+// Structure to store filename and size
+struct FileData {
+    std::string filename;
+    long long size; // Using long long to store large file sizes
+};
 
+// Function to generate the list of files and their sizes
 std::string getFileList(const std::string& directory) {
     std::string fileListStr;
     for (const auto& entry : fs::directory_iterator(directory)) {
         if (fs::is_regular_file(entry)) {
-            fileListStr += entry.path().filename().string() + " " + std::to_string(fs::file_size(entry)) + "\n";
+            FileData fileData;
+            fileData.filename = entry.path().filename().string();
+            fileData.size = fs::file_size(entry);
+            fileListStr += fileData.filename + " " + std::to_string(fileData.size) + "\n";
         }
     }
     return fileListStr;
 }
 
-void sendFileListToServer(int sockfd, const std::string& folderPath) {
-    std::string fileListStr = getFileList(folderPath);
-    ssize_t bytesSent = send(sockfd, fileListStr.c_str(), fileListStr.length(), 0);
-    if (bytesSent == -1) {
-        perror("send");
-        syslog(LOG_ERR, "Erreur lors de l'envoi de la liste des fichiers au serveur.");
-    } else {
-        syslog(LOG_INFO, "Liste des fichiers envoyée au serveur avec succès.");
-    }
+// Fonction pour recevoir et afficher les données du serveur
+void receiveAndDisplayData(int client_socket) {
+    char buffer[1024] = {0};
+    read(client_socket, buffer, sizeof(buffer));
+    std::cout << "Data received from server:\n" << buffer << std::endl;
 }
 
-int main(int argc , char *argv[]) {
-    openlog("client", LOG_PID | LOG_CONS, LOG_USER); // Ouvrir une connexion vers le système de logging
+// Function to send data to the server
+void sendDataToServer(int clientSocket, const std::string& data) {
+    send(clientSocket, data.c_str(), data.size(), 0);
+}
 
-    std::string folderPath = "/home/bonice/Bureau/Master 1/systeme distribues/projet3/data";
-    const char* serverIP = "127.0.0.1";
-    int serverPort = 8082;
-
+int main(int argc, char *argv[]) {
     int opt;
-    while ((opt = getopt(argc, argv, "i:p:d:")) != -1) {
+    std::string serverIP;
+    int serverPort = 0; // Initialize to zero to check later
+    std::string filename; // Declare filename outside switch block
+
+    // Parsing command line options
+    while ((opt = getopt(argc, argv, "i:p:")) != -1) {
         switch (opt) {
             case 'i':
                 serverIP = optarg;
                 break;
             case 'p':
-                serverPort = std::stoi(optarg);
-                break;
-            case 'd':
-                folderPath = optarg;
+                serverPort = atoi(optarg);
                 break;
             default:
-                std::cerr << "Usage: " << argv[0] << " [-i server_ip] [-p server_port] [-d folder_path]" << std::endl;
-                return 1;
+                std::cerr << "Usage: " << argv[0] << " -i <server IP> -p <port>\n";
+                return -1;
         }
     }
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("socket");
-        syslog(LOG_ERR, "Erreur lors de la création de la socket.");
-        closelog();
-        return 1;
+    if (serverIP.empty() || serverPort == 0) {
+        std::cerr << "Server IP address or port missing or invalid.\n";
+        return -1;
     }
 
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(serverPort);
-    if (inet_pton(AF_INET, serverIP, &serv_addr.sin_addr) <= 0) {
-        perror("inet_pton");
-        syslog(LOG_ERR, "Erreur lors de la conversion de l'adresse IP.");
-        close(sockfd);
+    openlog("client", LOG_PID, LOG_USER);
+
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); // Create socket
+    if (clientSocket == -1) {
+        std::cerr << "Error: Failed to create socket\n";
+        syslog(LOG_ERR, "Error: Failed to create socket");
         closelog();
-        return 1;
+        return -1;
     }
 
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connect");
-        syslog(LOG_ERR, "Erreur lors de la connexion au serveur.");
-        close(sockfd);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);
+    if (inet_pton(AF_INET, serverIP.c_str(), &serverAddr.sin_addr) <= 0) {
+        std::cerr << "Error: Invalid address or address not supported\n";
+        syslog(LOG_ERR, "Error: Invalid address or address not supported");
+        close(clientSocket);
         closelog();
-        return 1;
-    }
-    
-    sendFileListToServer(sockfd, folderPath);
-
-    int fd = inotify_init();
-    if (fd < 0) {
-        perror("inotify_init");
-        syslog(LOG_ERR, "Erreur lors de l'initialisation de inotify.");
-        close(sockfd);
-        closelog();
-        return 1;
+        return -1;
     }
 
-    int wd = inotify_add_watch(fd, folderPath.c_str(), IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVED_FROM | IN_MOVED_TO);
-    if (wd == -1) {
-        perror("inotify_add_watch");
-        syslog(LOG_ERR, "Erreur lors de l'ajout de la surveillance inotify.");
-        close(fd);
-        close(sockfd);
+    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error: Connection failed\n";
+        syslog(LOG_ERR, "Error: Connection failed");
+        close(clientSocket);
         closelog();
-        return 1;
+        return -1;
     }
 
-    char buffer[BUF_LEN];
+    std::string directory = "data";
+    const char *message = "Send data";
+
     while (true) {
-        // Vérifier la connexion à chaque itération
-        if (sockfd < 0 || (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)) {
-            perror("Reconnecting...");
-            syslog(LOG_WARNING, "Tentative de reconnexion au serveur...");
-            close(sockfd);
-            sockfd = socket(AF_INET, SOCK_STREAM, 0);
-            if (sockfd < 0) {
-                perror("socket");
-                syslog(LOG_ERR, "Erreur lors de la création de la socket.");
-                closelog();
-                return 1;
-            }
-            if (inet_pton(AF_INET, serverIP, &serv_addr.sin_addr) <= 0) {
-                perror("inet_pton");
-                syslog(LOG_ERR, "Erreur lors de la conversion de l'adresse IP.");
-                close(sockfd);
-                closelog();
-                return 1;
-            }
-            continue;
-        }
+        // Generate the new list of files
+        std::string currentFileListStr = getFileList(directory);
 
-        int length = read(fd, buffer, BUF_LEN);
-        if (length < 0) {
-            perror("read");
-            syslog(LOG_ERR, "Erreur lors de la lecture des événements inotify.");
-            break;
-        }
+        // Display menu options
+        std::cout << "\nMenu:\n";
+        std::cout << "1. present list file\n";
+        std::cout << "2. see file list\n";
+        std::cout << "3. Download file from server\n";
+        std::cout << "4. Exit\n";
+        std::cout << "Enter your choice: ";
 
-        int i = 0;
-        while (i < length) {
-            struct inotify_event *event = (struct inotify_event *)&buffer[i];
-            if (event->len) {
-                if (event->mask & IN_CREATE || event->mask & IN_DELETE || event->mask & IN_MODIFY || event->mask & IN_MOVED_FROM || event->mask & IN_MOVED_TO) {
-                    sendFileListToServer(sockfd, folderPath);
-                }
-            }
-            i += sizeof(struct inotify_event) + event->len;
+        int choice;
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                // Send the new file list to the server
+                sendDataToServer(clientSocket, currentFileListStr);
+                break;
+            case 2:
+                // Send request to server
+                sendDataToServer(clientSocket, message);
+                // Receive and display data from server
+                receiveAndDisplayData(clientSocket);
+                break;
+            case 3:
+              
+                break;
+            case 4:
+                // Exit
+                std::cout << "Exiting program.\n";
+                close(clientSocket);
+                closelog();
+                return 0;
+            default:
+                std::cerr << "Invalid choice. Please enter a number between 1 and 4.\n";
         }
     }
 
-    inotify_rm_watch(fd, wd);
-    close(fd);
-    close(sockfd);
-
-    closelog(); // Fermer la connexion vers le système de logging
-
+    close(clientSocket);
+    closelog();
     return 0;
 }
 
